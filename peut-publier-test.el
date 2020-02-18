fb2a98096394f4559c199fd05ec68e18ee4e8c3e
Matt Trzcinski <mtrzcinski@sealandaire.com>
2020-02-18 14:39:59 -0500

    Begin work on renderer

--- peut-publier-test.el
+++ peut-publier-test.el
@@ -4,7 +4,16 @@
 
 ;;; Code:
 
+(require 'peut-publier)
 (require 'ert)
+
+;; "Sample post file without meta information."
+(setq peut-publier-test-post-content
+      (concat "Hello, world!"))
+
+(ert-deftest peut-publier-render-to-html ()
+  "Test the main rendering function."
+  (should (eq (type-of (peut-publier-render-to-html)) 'string))
 
 (provide 'peut-publier-test)
 
--- peut-publier.el
+++ peut-publier.el
@@ -10,6 +10,28 @@
 
 ;;; Code:
 
+
+;; (setq my-file "~/test-post.org")
+;; (funcall peut-publier-default-renderer my-file)
+(setq peut-publier-default-renderer
+      (lambda (file)
+	(let* ((org-export-with-toc nil)
+	       (org-export-with-section-numbers nil)
+	       (converted
+		(with-temp-buffer
+		  (insert-file-contents-literally file)
+		  (org-export-as 'html nil nil t nil))))
+	  converted)))
+
+(defun peut-publier-render-to-html (file &optional renderer)
+  "Return html string conversion of FILE, using RENDERER.
+
+The default RENDERER is `org-export-as'.  The user may provide
+their own RENDERER.  RENDERER must be a function which accepts a
+file and returns a string of html."
+
+  )
+
 (provide 'peut-publier)
 
 ;;; peut-publier.el ends here
