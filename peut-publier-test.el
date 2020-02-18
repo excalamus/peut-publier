81b7d2ede5e2b00d25662cb0ee3d9d3d6b289724
Excalamus <excalamus@tutanota.com>
2020-02-17 19:25:04 -0500

    Create main and test fild

---
+++ peut-publier-test.el
@@ -0,0 +1,11 @@
+;;; peut-publier-test.el --- test suite for peut-publier.el -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'ert)
+
+(provide 'peut-publier-test)
+
+;;; peut-publier-test.el ends here
---
+++ peut-publier.el
@@ -0,0 +1,15 @@
+;; peut-publier.el --- (P)eut-publier (E)nables (U)sers (T)o... Publish -*- lexical-binding: t; -*-
+
+;; Author: Matt Trzcinski
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "26.3"))
+;; Keywords:  static, website, generator, blog, publish
+;; URL: https://github.com/excalamus/peut-publier
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'peut-publier)
+
+;;; peut-publier.el ends here
