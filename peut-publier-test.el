24bbd21e379d4e737864d9501ec34d23ea8fe7ac
Excalamus <excalamus@tutanota.com>
2020-03-10 13:42:32 -0400

    Update render caller and implement default renderer

--- peut-publier-test.el
+++ peut-publier-test.el
@@ -7,16 +7,136 @@
 (require 'ert)
 (require 'peut-publier)
 
-;; "Sample post file without meta information."
-(setq peut-publier-test-post-content
-      (concat "Hello, world!\n"))
+;;; Helpers
 
-(ert-deftest peut-publier-render-to-html ()
-  "Test the main rendering function."
-  (let ((temp-file (concat (temporary-file-directory) "dummy-post")))
-    (with-temp-file temp-file
-      (insert peut-publier-test-post-content))
-      (should (eq (type-of (peut-publier-render-to-html temp-file)) 'string))))
+(defmacro redefvar (symbol newval &optional doc)
+  "Reset the value of a symbol which was created using `defvar'.
+This function is intended only for development purposes.  Simply
+replace `defvar' with `redefvar' and SYMBOL will be assigned
+NEWVAL.  DOC will be ignored."
+  `(setq ,symbol ,newval))
+
+;;; Variables
+
+(defvar peut-publier-test-post-content
+  (concat
+   "* Header\n"
+   "** Subheader\n"
+   "Hello, world!\n\n"
+    ;; #+begin_src creates inline-css,
+    ;; #+begin_example does not
+   "#+begin_src python\n"
+   "    print('Goodbye, cruel world...')\n"
+   "#+end_src\n")
+  "Sample post file without meta information.")
+
+;;; Tests
+
+(ert-deftest peut-publier-test-render-to-html ()
+  "Test that `peut-publier-render-to-html' accepts a file and
+returns a string.  Use `peut-publier-default-renderer'."
+(let* ((test-file (concat (temporary-file-directory) "test-file"))
+       (result (progn
+                 (with-temp-file test-file
+                   (insert peut-publier-test-post-content))
+                 (peut-publier-render-to-html test-file))))
+  (delete-file test-file)
+  (should (eq (type-of result) 'string))))
+
+(ert-deftest peut-publier-test-renderer-org-export-toc-nil ()
+  "Test that `peut-publier-renderer-org-export' does not output
+table of contents unless user passes t."
+  (let* ((test-file (concat (temporary-file-directory) "test-file"))
+         (result (progn
+                   (with-temp-file test-file
+                     (insert peut-publier-test-post-content))
+                   (peut-publier-renderer-org-export test-file nil))))
+    (delete-file test-file)
+    (should-not (string-match-p "id=\"table-of-contents\"" result))))
+
+(ert-deftest peut-publier-test-renderer-org-export-toc-t ()
+  "Test that `peut-publier-renderer-org-export' outputs table of
+contents when t."
+  (let* ((test-file (concat (temporary-file-directory) "test-file"))
+         (result (progn
+                   (with-temp-file test-file
+                     (insert peut-publier-test-post-content))
+                   (peut-publier-renderer-org-export test-file t))))
+    (delete-file test-file)
+    (should (string-match-p "id=\"table-of-contents\"" result))))
+
+(ert-deftest peut-publier-test-renderer-org-export-section-num-nil ()
+  "Test that `peut-publier-renderer-org-export' does not output
+section numbers unless user passes t."
+  (let* ((test-file (concat (temporary-file-directory) "test-file"))
+         (result (progn
+                   (with-temp-file test-file
+                     (insert peut-publier-test-post-content))
+                   (peut-publier-renderer-org-export test-file nil nil))))
+    (delete-file test-file)
+    (should-not (string-match-p "class=\"section-number" result))))
+
+(ert-deftest peut-publier-test-renderer-org-export-section-num-t ()
+  "Test that `peut-publier-renderer-org-export' does not output
+section numbers unless user passes t."
+  (let* ((test-file (concat (temporary-file-directory) "test-file"))
+         (result (progn
+                   (with-temp-file test-file
+                     (insert peut-publier-test-post-content))
+                   (peut-publier-renderer-org-export test-file nil t))))
+    (delete-file test-file)
+    (should (string-match-p "class=\"section-number" result))))
+
+(ert-deftest peut-publier-test-renderer-org-export-inline-css ()
+  "Test that `peut-publier-renderer-org-export' output type 'inline-css
+generates inline-css."
+  (let* ((test-file (concat (temporary-file-directory) "test-file"))
+         (result (progn
+                   (with-temp-file test-file
+                     ;; #+begin_src creates inline-css,
+                     ;; #+begin_example does not
+                     (insert peut-publier-test-post-content))
+                   (peut-publier-renderer-org-export test-file nil nil 'inline-css))))
+    (delete-file test-file)
+    (should (string-match-p (regexp-quote "span style=") result))))
+
+(ert-deftest peut-publier-test-renderer-org-export-css ()
+  "Test that `peut-publier-renderer-org-export' output type 'css
+does not generate inline-css."
+  (let* ((test-file (concat (temporary-file-directory) "test-file"))
+         (result (progn
+                   (with-temp-file test-file
+                     ;; #+begin_src creates inline-css,
+                     ;; #+begin_example does not
+                     (insert peut-publier-test-post-content))
+                   (peut-publier-renderer-org-export test-file nil nil 'css))))
+    (delete-file test-file)
+    (should-not (string-match-p (regexp-quote "span style=") result))))
+
+(ert-deftest peut-publier-test-renderer-org-export-plain-text ()
+  "Test that `peut-publier-renderer-org-export' output type nil
+does not generate style ('inline-css) or class ('css) span tags."
+  (let* ((test-file (concat (temporary-file-directory) "test-file"))
+         (result (progn
+                   (with-temp-file test-file
+                     ;; #+begin_src creates inline-css,
+                     ;; #+begin_example does not
+                     (insert peut-publier-test-post-content))
+                   (peut-publier-renderer-org-export test-file nil nil 'plain-text))))
+    (delete-file test-file)
+    (should-not (string-match-p (regexp-quote "span style=") result))
+    (should-not (string-match-p (regexp-quote "span class=") result))))
+
+(ert-deftest peut-publier-test-renderer-org-export-default-backend ()
+  "Test that `peut-publier-renderer-org-export' default backend
+is 'html."
+  (let* ((test-file (concat (temporary-file-directory) "test-file"))
+         (result (progn
+                   (with-temp-file test-file
+                     (insert peut-publier-test-post-content))
+                   (peut-publier-renderer-org-export test-file nil nil nil nil))))
+    (delete-file test-file)
+    (should (string-match-p (regexp-quote "</div>") result))))
 
 (provide 'peut-publier-test)
 
--- peut-publier.el
+++ peut-publier.el
@@ -11,42 +11,47 @@
 ;;; Code:
 
 (require 'org)
+(require 'ox) ;; org-html-htmlize-output-type
 
-;; (setq my-file "~/test-post.org")
-;; (setq my-file "/data/data/com.termux/files/home/site/post/2020-01-20-everything-in-org.org")
-;; (funcall peut-publier-default-renderer my-file)
-
-(defun peut-publier-renderer-org-export (file &optional toc section-num backend)
+(defun peut-publier-renderer-org-export (file &optional toc section-num output-type backend)
   "Export FILE to html string using `org-export-as'.
 
+This function wraps `org-export-as'.  See that function for more
+details about the arguments.
+
 TOC and SECTION-NUM generate table of contents and section
-numbers, respectively.  Defaults for each are nil.  BACKEND is
-the export backend to use.  Default is 'html."
+numbers, respectively.  Defaults for each are nil.
+
+OUTPUT-TYPE is 'css, 'inline-css, or nil as defined by
+`org-html-htmlize-output-type'.  Default is 'css.
+
+BACKEND is the export backend.  Default is 'html."
   (let* ((org-export-with-toc toc)
-         (org-export-with-section-numbers section-num)
-         (backend (or backend 'html))
-         (converted
-          (with-temp-buffer
-            (insert-file-contents-literally file)
-            (org-export-as backend nil nil t nil))))
-    converted))
+	 (org-export-with-section-numbers section-num)
+	 ;; To toggle nil, user must specify 'plain-text
+	 (org-html-htmlize-output-type
+	  (cond ((eq output-type 'css) output-type)
+		((eq output-type 'inline-css) output-type)
+		((eq output-type 'plain-text) nil)
+		('css)))
+	 (backend (or backend 'html)))
+    (with-temp-buffer
+      (insert-file-contents-literally file)
+      (org-export-as backend nil nil t nil))))
 
-(defvar peut-publier-default-renderer 'peut-publier-renderer-org-export
-  "Default renderer to be used with `peut-publier-render-to-html'.")
+(defvar peut-publier-default-renderer #'peut-publier-renderer-org-export
+  "Default renderer function to be used with
+`peut-publier-render-to-html'.")
 
-;; (setq peut-publier-default-renderer 'peut-publier-renderer-org-export)
-
-
-(defun peut-publier-render-to-html (file &optional renderer)
-  "Return html string conversion of FILE using RENDERER.
+(defun peut-publier-render-to-html (file &optional renderer &rest rargs)
+  "Return html string conversion of FILE using RENDERER called with RARGS.
 
 The default RENDERER is `peut-publier-default-renderer'.  The
-user may provide their own RENDERER.  RENDERER must be a function
-which accepts a file and returns a string of html."
+user may provide their own RENDERER.  A RENDERER is a function
+which accepts a file, returns an html string, and may accept
+RARGS."
   (let ((renderer (or renderer peut-publier-default-renderer)))
-    (funcall renderer file)))
-
-
+    (apply renderer file rargs)))
 
 (provide 'peut-publier)
 
