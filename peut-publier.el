5e8dff72dd4dad8a0ba04edd976b83f1738c2be2
Excalamus <excalamus@tutanota.com>
2020-07-20 09:36:38 -0400

    Change favicon to .ico

--- peut-publier.el
+++ peut-publier.el
@@ -129,11 +129,11 @@ The key is page type and the value the associated template.")
 
 (defvar peut-publier-static-head
   (concat "     <meta charset=\"UTF-8\">\n"
-          "      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
-          "      <meta name=\"author\" content=\"Matt Trzcinski\">\n"
-          "      <meta name=\"referrer\" content=\"no-referrer\">\n"
-          "      <link href=\"static/style.css\" rel=\"stylesheet\" type=\"text/css\" />\n"
-          "      <link rel='shortcut icon' type=\"image/png\" href=\"static/favicon.png\" />\n")
+	  "      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+	  "      <meta name=\"author\" content=\"Matt Trzcinski\">\n"
+	  "      <meta name=\"referrer\" content=\"no-referrer\">\n"
+	  "      <link href=\"static/style.css\" rel=\"stylesheet\" type=\"text/css\" />\n"
+	  "      <link rel='shortcut icon' type=\"image/png\" href=\"static/favicon.ico\" />\n")
   "Part of <head> which is always the same.")
 
 (defun peut-publier-variable-head (page-path)
@@ -143,31 +143,31 @@ The key is page type and the value the associated template.")
 
 (defvar peut-publier-body-preamble
   (concat "   <div id=\"preamble\" class=\"status\">\n"
-          "      <nav>\n"
-          "         <div class=\"flexcontainer\">\n"
-          "            <div class=\"smallitem\">\n"
-          "               <ul class=\"inline-list\">\n"
-          "                  <li>Excalamus</li>\n"
-          "               </ul>\n"
-          "            </div>\n"
-          "            <div class=\"bigitem\">\n"
-          "               <ul class=\"inline-list\">\n"
-          "                  <li><a href=\".\">Home</a></li>\n"
-          "                  <li><a href=\"about.html\">About</a></li>\n"
-          "               </ul>\n"
-          "            </div>\n"
-          "         </div>\n"
-          "      </nav>\n"
-          "      <hr/>\n"
-          "   </div>\n")
+	  "      <nav>\n"
+	  "         <div class=\"flexcontainer\">\n"
+	  "            <div class=\"smallitem\">\n"
+	  "               <ul class=\"inline-list\">\n"
+	  "                  <li>Excalamus</li>\n"
+	  "               </ul>\n"
+	  "            </div>\n"
+	  "            <div class=\"bigitem\">\n"
+	  "               <ul class=\"inline-list\">\n"
+	  "                  <li><a href=\".\">Home</a></li>\n"
+	  "                  <li><a href=\"about.html\">About</a></li>\n"
+	  "               </ul>\n"
+	  "            </div>\n"
+	  "         </div>\n"
+	  "      </nav>\n"
+	  "      <hr/>\n"
+	  "   </div>\n")
   "Section which appears above the content in <body>.")
 
 (defvar peut-publier-body-postamble
   (concat "   <div id=\"postamble\" class=\"status\">\n"
-          "      <hr/>\n"
-          "      <p>Powered by <a href=\"https://github.com/excalamus/peut-publier\">peut-publier</a></p>\n"
-          "      <p>©2020 Excalamus.com</p>\n"
-          "    </div>\n")
+	  "      <hr/>\n"
+	  "      <p>Powered by <a href=\"https://github.com/excalamus/peut-publier\">peut-publier</a></p>\n"
+	  "      <p>©2020 Excalamus.com</p>\n"
+	  "    </div>\n")
   "Section which appears below the content in <body>.")
 
 (defvar peut-publier-default-renderer #'peut-publier-render-org-to-html
@@ -214,7 +214,7 @@ END correspond to `peut-publier-meta-data-start' and
     (insert-file-contents-literally file)
     (goto-char (point-min))
     (let* ((start (or start (search-forward-regexp peut-publier-meta-data-start)))
-           (end (or end (search-forward-regexp peut-publier-meta-data-end))))
+	   (end (or end (search-forward-regexp peut-publier-meta-data-end))))
       (delete-region start end)
       (buffer-string))))
 
@@ -227,7 +227,7 @@ Default START and END correspond to
     (insert-file-contents-literally file)
     (goto-char (point-min))
     (let* ((start (or start (search-forward-regexp peut-publier-meta-data-start)))
-           (end (or end (search-forward-regexp peut-publier-meta-data-end))))
+	   (end (or end (search-forward-regexp peut-publier-meta-data-end))))
       (buffer-substring-no-properties start end))))
 
 (defun peut-publier-parse-org-meta-data (data)
@@ -240,7 +240,7 @@ case-sensitive!.  Values are whatever remains on that line."
     (insert data)
     (org-element-map (org-element-parse-buffer 'element) 'keyword
       (lambda (x) (cons (org-element-property :key x)
-                        (org-element-property :value x))))))
+			(org-element-property :value x))))))
 
 (defun peut-publier-get-meta-data-alist (file &optional parser)
   "Return FILE meta-data as alist using PARSER.
@@ -248,7 +248,7 @@ case-sensitive!.  Values are whatever remains on that line."
 The `peut-publier-default-meta-data-parser' is used no PARSER is
 provided."
   (let ((meta-data (peut-publier-get-meta-data file))
-         (parser (or parser peut-publier-default-meta-data-parser)))
+	 (parser (or parser peut-publier-default-meta-data-parser)))
     (funcall parser meta-data)))
 
 
@@ -277,9 +277,9 @@ supported by peut-publier.
 ;; defaults and cannot be nil. See
 ;; https://emacs.stackexchange.com/questions/55684/
   (let* ((org-export-with-toc toc)
-         (org-export-with-section-numbers section-num)
-         (org-html-htmlize-output-type output-type)
-         (backend (or backend peut-publier--default-org-backend)))
+	 (org-export-with-section-numbers section-num)
+	 (org-html-htmlize-output-type output-type)
+	 (backend (or backend peut-publier--default-org-backend)))
     (with-temp-buffer
       (insert string)
       (org-export-as backend nil nil t nil))))
@@ -292,7 +292,7 @@ user may provide their own RENDERER.  A RENDERER is a function
 which accepts a file, returns an html string, and may accept
 RARGS."
   (let ((renderer (or renderer peut-publier-default-renderer))
-        (content (peut-publier-strip-meta-data file)))
+	(content (peut-publier-strip-meta-data file)))
     (apply renderer content rargs)))
 
 
@@ -301,8 +301,8 @@ RARGS."
 (defun peut-publier-post-template (page-path)
   "Insert PAGE-PATH into html string template."
   (let* ((meta-data (peut-publier-get-meta-data-alist page-path))
-         (toc (peut-publier-alist-get "TOC" meta-data))
-         (body-content (peut-publier-render-to-html page-path peut-publier-default-renderer toc)))
+	 (toc (peut-publier-alist-get "TOC" meta-data))
+	 (body-content (peut-publier-render-to-html page-path peut-publier-default-renderer toc)))
     (concat
      "\n<div id=\"content\">\n"
      "<h1>" (peut-publier-alist-get "TITLE" meta-data) "</h1>\n"
@@ -315,20 +315,20 @@ RARGS."
 (defun peut-publier-about-template (page-path)
   "Insert PAGE-PATH into html string template."
   (let* ((meta-data (peut-publier-get-meta-data-alist page-path))
-         (body-content (peut-publier-render-to-html page-path)))
-            (concat
-             "<div id=\"content\">\n"
-             "<img id=\"img-float\" src=\""
-             peut-publier-about-img "\" alt=\""
-             peut-publier-about-img-alt "\">\n"
-             "<h1 class=\"title\">" (peut-publier-alist-get "TITLE" meta-data) "</h1>\n"
-             body-content
-             "</div>\n")))
+	 (body-content (peut-publier-render-to-html page-path)))
+	    (concat
+	     "<div id=\"content\">\n"
+	     "<img id=\"img-float\" src=\""
+	     peut-publier-about-img "\" alt=\""
+	     peut-publier-about-img-alt "\">\n"
+	     "<h1 class=\"title\">" (peut-publier-alist-get "TITLE" meta-data) "</h1>\n"
+	     body-content
+	     "</div>\n")))
 
 (defun peut-publier-index-template (page-path)
   "Insert PAGE-PATH into html string template."
   (let* ((meta-data (peut-publier-get-meta-data-alist page-path))
-         (body-content (peut-publier-render-to-html page-path)))
+	 (body-content (peut-publier-render-to-html page-path)))
     (concat
      "<div id=\"content\">\n"
      body-content
@@ -340,28 +340,28 @@ RARGS."
 (defun peut-publier-assemble-page (page-path)
   "Assemble PAGE-PATH into final html string."
   (let* ((meta-data (peut-publier-get-meta-data-alist page-path))
-         (mtype (peut-publier-alist-get "TYPE" meta-data))
-         (type (cond ((stringp mtype) (intern mtype))    ; convert strings to symbols
-                     ((and (symbolp mtype) mtype) mtype) ; not nil
-                     (t 'post)))                         ; default is 'post
-         (template (peut-publier-alist-get type peut-publier--template-alist))
-         (page-content (funcall template page-path)))
+	 (mtype (peut-publier-alist-get "TYPE" meta-data))
+	 (type (cond ((stringp mtype) (intern mtype))    ; convert strings to symbols
+		     ((and (symbolp mtype) mtype) mtype) ; not nil
+		     (t 'post)))                         ; default is 'post
+	 (template (peut-publier-alist-get type peut-publier--template-alist))
+	 (page-content (funcall template page-path)))
     (when page-content
       (with-temp-buffer
-        (insert (concat
-                 "<!DOCTYPE html5>\n"
-                 "<html lang=\"en\">\n"
-                 "   <head>\n"
-                 peut-publier-static-head
-                 (peut-publier-variable-head page-path)
-                 "   </head>\n"
-                 "   <body>\n"
-                 peut-publier-body-preamble
-                 page-content
-                 peut-publier-body-postamble
-                 "   </body>\n"
-                 "</html>"))
-        (buffer-string)))))
+	(insert (concat
+		 "<!DOCTYPE html5>\n"
+		 "<html lang=\"en\">\n"
+		 "   <head>\n"
+		 peut-publier-static-head
+		 (peut-publier-variable-head page-path)
+		 "   </head>\n"
+		 "   <body>\n"
+		 peut-publier-body-preamble
+		 page-content
+		 peut-publier-body-postamble
+		 "   </body>\n"
+		 "</html>"))
+	(buffer-string)))))
 
 
 ;; Publish:
@@ -371,10 +371,10 @@ RARGS."
 
 Optionally change extension to EXTENSION."
   (let ((relative-path (concat
-                        (file-name-as-directory dir)
-                        (file-name-nondirectory path))))
+			(file-name-as-directory dir)
+			(file-name-nondirectory path))))
     (if extension
-        (concat (file-name-sans-extension relative-path) extension)
+	(concat (file-name-sans-extension relative-path) extension)
       relative-path)))
 
 (defun peut-publier-publish-page (page-path &optional out-dir)
@@ -383,12 +383,12 @@ Optionally change extension to EXTENSION."
 The `peut-publier-publish-directory' is used when no OUT-DIR is
 given."
   (let ((out-dir (or out-dir peut-publier-publish-directory))
-        (out-file (peut-publier-relative-to out-dir page-path ".html")))
+	(out-file (peut-publier-relative-to out-dir page-path ".html")))
     (condition-case nil
-        (progn
-          (with-temp-file out-file
-            (insert (peut-publier-assemble-page page-path)))
-          (message "Wrote %s" out-file))
+	(progn
+	  (with-temp-file out-file
+	    (insert (peut-publier-assemble-page page-path)))
+	  (message "Wrote %s" out-file))
       (error "Could not publish %s" page-path))))
 
 (defun peut-publier-dir-list (dir &optional include exclude)
@@ -397,7 +397,7 @@ given."
 INCLUDE or EXCLUDE files matching regexp.  Exclude happens after
 include."
     (mapcar (lambda (x) (concat dir x))
-            (seq-difference (directory-files dir nil include) exclude)))
+	    (seq-difference (directory-files dir nil include) exclude)))
 
 (defun peut-publier-html-page-list (&optional dir)
   "Return html list of pages in DIR."
@@ -405,10 +405,10 @@ include."
     (mapconcat
      (lambda (page-path)
        (concat
-        "   <li><p class=\"post-title\"><a href=\""
-        (peut-publier-relative-to peut-publier-tld page-path ".html") "\">"
-        (peut-publier-alist-get "TITLE" (peut-publier-get-meta-data-alist page-path))
-        "</a></p></li>\n"))
+	"   <li><p class=\"post-title\"><a href=\""
+	(peut-publier-relative-to peut-publier-tld page-path ".html") "\">"
+	(peut-publier-alist-get "TITLE" (peut-publier-get-meta-data-alist page-path))
+	"</a></p></li>\n"))
      (peut-publier-dir-list dir "\\.org$" '("about.org" "index.org"))
      "")))
 
@@ -423,10 +423,10 @@ Unless OUT-DIR, publish pages to
 `peut-publier-publish-directory'."
   (interactive)
   (let ((list (or list (peut-publier-dir-list peut-publier-src-directory)))
-        (out-dir (or out-dir peut-publier-publish-directory)))
+	(out-dir (or out-dir peut-publier-publish-directory)))
     (mapc (lambda (x)
-            (funcall  #'peut-publier-publish-page x out-dir))
-          list)
+	    (funcall  #'peut-publier-publish-page x out-dir))
+	  list)
     (message "Site rendered successfully")))
 
 (provide 'peut-publier)
